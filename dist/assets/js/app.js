4dd766a2c16758bb3826c4b37179e78b414c18d4
pavlenkonataliia <nataliiapavlenkonet@gmail.com>
2020-01-22 11:25:49 +0200

    init

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -13367,7 +13367,7 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! d:\FrontEnd\HomeWork\pets-hotel\src\assets\js\app.js */"./src/assets/js/app.js");
+module.exports = __webpack_require__(/*! d:\Beetroot\Beetroot_html\project-2\pets-hotel\src\assets\js\app.js */"./src/assets/js/app.js");
 
 
 /***/ })
