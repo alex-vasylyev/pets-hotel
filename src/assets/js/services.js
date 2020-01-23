d82bb9fd7d2116b753af2d16d4446da1061e4746
AleksQA <aleksandr.vasylyevqa@gmail.com>
2020-01-23 09:02:14 +0200

    !

---
+++ src/assets/js/services.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// console.log('HeLLO');
+
+
+// let url = 'data/employees.json';
+// fetch(url)
+// 	.then(response => response.json())
+// 	.then(showServices);
+
+// 	function showServices(data){
+// 		console.log(data);
+		
+// 	}
